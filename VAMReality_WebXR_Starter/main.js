14b0c93a715bf2fffa6626a6bacf41fe8c1d6f94
mbarange <mbarange@cesi.fr>
2025-06-29 11:46:54 +0200

    update

--- VAMReality_WebXR_Starter/main.js
+++ VAMReality_WebXR_Starter/main.js
@@ -1,9 +1,8 @@
 
-import { initializeStepBuilder } from './StepBuilderUI.js';
 import { initializeScenarioManager } from './ScenarioManager.js';
+import { initializeStepBuilder } from './StepBuilderUI.js';
 
 window.onload = () => {
   initializeScenarioManager();
   initializeStepBuilder();
-  
 };
