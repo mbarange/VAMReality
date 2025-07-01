8393e70e142a5a48a69f9efd07e05691dbc262e4
mbarange <mbarange@cesi.fr>
2025-07-01 14:04:49 +0200

    Revert "Update main.js"
    This reverts commit 3ec4bd712f1a906f1066ee3feaffc2468a03b5db.

--- VAMReality_WebXR_Starter/main.js
+++ VAMReality_WebXR_Starter/main.js
@@ -1,7 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // All your JS logic goes here, after the DOM is ready
-    initializeScenarioManager();
-    renderFlowGraph();
-  });
+
 // Optional: main.js for future global logic
 console.log("📦 VAMReality main.js loaded.");
