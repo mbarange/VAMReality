d720e7aa6b893974bd0f01f551671699785fad6a
mbarange <mbarange@cesi.fr>
2025-07-02 19:05:54 +0200

    Update GitHubSaver.js

--- VAMReality_WebXR_Starter/GitHubSaver.js
+++ VAMReality_WebXR_Starter/GitHubSaver.js
@@ -18,7 +18,7 @@ window.saveToGitHub = async function () {
   console.log("📦 Saving scenario: ", scenario);
 
   const path = folder ? `${folder}/${scenario.name}.json` : `${scenario.name}.json`;
-  const apiUrl = `https://api.github.com/repos/${user}/${repo}/main/${path}`;
+  const apiUrl = `https://api.github.com/repos/${user}/${repo}/contents/${path}`;
 
   try {
     const check = await fetch(apiUrl, {
@@ -74,9 +74,8 @@ window.loadFromGitHub = async function () {
     return;
   }
 
-  const apiUrl =`https://api.github.com/repos/${user}/${repo}/main/${folder}?t=${Date.now()}`;
 
- // const apiUrl = "https://api.github.com/repos/" + user + "/" + repo + "/contents/" + folder?t=${Date.now()}`;
+  const apiUrl = "https://api.github.com/repos/" + user + "/" + repo + "/contents/" + folder?t=${Date.now()}`;
 
   try {
     const res = await fetch(apiUrl, {
